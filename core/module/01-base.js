448ca37ede8c63b75b27529881e4bb23b78b06d2
banli17 <867889876@qq.com>
2022-08-14 22:43:57 +0800

    update

--- core/module/01-base.js
+++ core/module/01-base.js
@@ -1,4 +1,7 @@
-// const m = require('./m')
 const fs = require('fs')
+let m = require('./m')
+m = require('./m')
 
 console.log('执行我')
+
+console.log(fs.readFileSync(__dirname + '/v.js'))
--- debugger-main.js
+++ debugger-main.js
@@ -1,2 +1,4 @@
-import './core/stream/12-write-drain.js'
+// import './core/stream/12-write-drain.js'
 // import './core/stream/13-drain-speed.js'
+
+import './core/module/01-base.js'
